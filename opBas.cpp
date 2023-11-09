fc848325c87c2644e81097d9763d4cba407f66a4
KravAD <dkrav@myuax.com>
2023-11-09 11:50:03 +0100

    commit2
    Declaración de los valores enteros y salida por pantalla.

--- opBas.cpp
+++ opBas.cpp
@@ -4,3 +4,8 @@ using namespace std;
 int suma(int a, int b) {
     return a + b;
 }
+
+int main() {
+    int a = 5, b = 3;
+    cout << "La suma de " << a <<" y" << b <<"es: " << suma(a, b) <<endl;
+}
