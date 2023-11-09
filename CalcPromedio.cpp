a9cef8dea88032ace4af080bcc8ac4d72f785620
KravAD <dkrav@myuax.com>
2023-11-09 12:52:09 +0100

    commit 13
    creacion de funcion de variable tipo double para calcular el promedio del estudiante

--- CalcPromedio.cpp
+++ CalcPromedio.cpp
@@ -2,3 +2,10 @@
 #include <vector>
 using namespace std;
 
+double calcular_promedio(const vector<double>& calificaciones) {
+    int suma = 0;
+    for (int calificacion : calificaciones) {
+        suma += calificacion;
+    }
+    return static_cast<double>(suma) / calificaciones.size();
+}
