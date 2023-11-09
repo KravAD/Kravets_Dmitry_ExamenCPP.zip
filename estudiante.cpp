687848a217d996859916b97625c022baea653be2
KravAD <dkrav@myuax.com>
2023-11-09 12:02:35 +0100

    commit 4
    atributos públicos y privados de clase estudiante (nombre, edad, y grado).

--- estudiante.cpp
+++ estudiante.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Estudiante {
+public:
+    estudiante (string nombre, int edad, string grado) : nombre_(nombre), edad_(edad), grado_(grado) {}
+    string nombre() const { return nombre_; }
+    int edad() const { return edad_;}
+    string grado() const { return grado_; }
+private:
+    string nombre_;
+    int edad_;
+}
+
+
+
+
