bf200bed68d3413816d0b32c97bfb7da23b5b59f
KravAD <dkrav@myuax.com>
2023-11-09 13:36:05 +0100

    commit 18
    no funciona el filtro de grado y dejo sin hacer la optimizacion de memoria (punto 3)

--- FiltroGrado.cpp
+++ FiltroGrado.cpp
@@ -1,13 +1,33 @@
-#include <iostream>
 #include <vector>
+#include <iostream>
+#include <string>
+#include "estudiante.h"
 using namespace std;
 
-vector<Estudiante> filtrar_por_grado(const vector<Estudiante>& estudiantes, string grado) {
-    vector<Estudiante> resultado;
+vector<estudiante> filtrar_por_grado(const vector<estudiante>& estudiantes, const string& grado) {
+    vector<estudiante> resultado;
     for (const auto& estudiante : estudiantes) {
-        if (estudiante.grado() == grado) {
-            resultado.pushback(estudiante);
+        if (estudiante.getGrado() == grado) {
+            resultado.push_back(estudiante);
         }
     }
     return resultado;
+}
+
+int main() {
+    vector<estudiante> estudiantes = {
+            estudiante("Juan", 20, "Ingeniería en Sistemas"),
+            estudiante("Ana", 19, "Ingeniería en Sistemas"),
+            estudiante("Pedro", 21, "Ingeniería Civil")
+    };
+
+    string grado = "Ingeniería en Sistemas";
+    vector<estudiante> filtrados = filtrar_por_grado(estudiantes, grado);
+
+    cout << "Estudiantes en " << grado << ":\n";
+    for (const auto& estudiante : filtrados) {
+        estudiante.mostrar_info();
+    }
+
+    return 0;
 }
