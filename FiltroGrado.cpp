2e65d304599e943b94317b902d0e846e3cefe49c
KravAD <dkrav@myuax.com>
2023-11-09 13:26:50 +0100

    commit 17
    vector condicioneado a los grados de los estudiantes que si se cumple retorna el grado de estos.

--- FiltroGrado.cpp
+++ FiltroGrado.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+vector<Estudiante> filtrar_por_grado(const vector<Estudiante>& estudiantes, string grado) {
+    vector<Estudiante> resultado;
+    for (const auto& estudiante : estudiantes) {
+        if (estudiante.grado() == grado) {
+            resultado.pushback(estudiante);
+        }
+    }
+    return resultado;
+}
